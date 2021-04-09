0ef870f98736748acec3a690bf5b0bbceac8a44a
aleksei.potapov <aleksei.potapov@doclerholding.com>
2021-04-09 14:58:03 +0200

    fix onclick

--- LuxCommunity/src/main/java/eu/gameofcode/endgame/service/EventService.java
+++ LuxCommunity/src/main/java/eu/gameofcode/endgame/service/EventService.java
@@ -20,26 +20,26 @@ public class EventService {
 
     @PostConstruct
     public void init() {
-        EventDto dto = new EventDto();
-        dto.setId(1);
-        dto.setLatitude("49.617844812170986");
-        dto.setLongitude("6.1404697460290985");
-        dto.setName("Art exhibition");
-        dto.setDescription("Art exhibition");
-        dto.setEventTime(1618999951239l);
-        dto.setOnline(false);
-
         EventDto dto1 = new EventDto();
-        dto.setId(2);
-        dto1.setLatitude("49.517844812170986");
-        dto1.setLongitude("6.2404697460290985");
-        dto1.setName("Art exhibition 2");
-        dto1.setDescription("Art exhibition 2");
+        dto1.setId(1);
+        dto1.setLatitude("49.617844812170986");
+        dto1.setLongitude("6.1404697460290985");
+        dto1.setName("Art exhibition 1");
+        dto1.setDescription("Art exhibition 1");
         dto1.setEventTime(1618999951239l);
         dto1.setOnline(false);
 
+        EventDto dto2 = new EventDto();
+        dto2.setId(2);
+        dto2.setLatitude("49.517844812170986");
+        dto2.setLongitude("6.2404697460290985");
+        dto2.setName("Art exhibition 2");
+        dto2.setDescription("Art exhibition 2");
+        dto2.setEventTime(1618999951239l);
+        dto2.setOnline(false);
+
         EventDto dto3 = new EventDto();
-        dto.setId(3);
+        dto3.setId(3);
         dto3.setLatitude("49.717844812170986");
         dto3.setLongitude("6.0404697460290985");
         dto3.setName("Art exhibition 3");
@@ -47,8 +47,8 @@ public class EventService {
         dto3.setEventTime(1618999951239l);
         dto3.setOnline(false);
 
-        eventDtos.add(dto);
         eventDtos.add(dto1);
+        eventDtos.add(dto2);
         eventDtos.add(dto3);
     }
 }

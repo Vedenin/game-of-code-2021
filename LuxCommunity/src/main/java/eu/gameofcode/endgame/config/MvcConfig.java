61c3e6816a9a3b171c051e7c06a9144e2ce4f320
aleksei.potapov <aleksei.potapov@doclerholding.com>
2021-04-09 12:33:41 +0200

    add about page

--- LuxCommunity/src/main/java/eu/gameofcode/endgame/config/MvcConfig.java
+++ LuxCommunity/src/main/java/eu/gameofcode/endgame/config/MvcConfig.java
@@ -8,6 +8,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class MvcConfig implements WebMvcConfigurer {
 
 	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/about").setViewName("about");
+		registry.addViewController("/contact").setViewName("contact");
 //		registry.addViewController("/home").setViewName("home");
 		//registry.addViewController("/").setViewName("home");
 		//registry.addViewController("/admin_panel").setViewName("admin_panel");

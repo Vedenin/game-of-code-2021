4921aeba9181c9da20691d184562660c7ebccc39
ievgenii.gerasimenko <ievgenii.gerasimenko@doclerholding.com>
2021-04-10 13:22:22 +0200

    add new event

--- LuxCommunity/src/main/java/eu/gameofcode/endgame/security/WebSecurityConfig.java
+++ LuxCommunity/src/main/java/eu/gameofcode/endgame/security/WebSecurityConfig.java
@@ -24,7 +24,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 				.permitAll()
 				.and()
 			.logout()
-				.permitAll();
+				.permitAll().and().csrf().disable();
 	}
 
 	@Bean
